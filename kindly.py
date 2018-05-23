e187425b7b37691115e920e3f39a934f5090272b
Philipp Hack <philipp.hack@gmail.com>
2018-05-23 11:30:49 +0200

    User logins

--- kindly.py
+++ kindly.py
@@ -1,16 +1,16 @@
-from contextlib import contextmanager
 from collections import OrderedDict
 from datetime import datetime, timedelta
 import functools
 import feedparser
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_bootstrap import Bootstrap
 from flask_bootstrap.nav import BootstrapRenderer
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_nav import Nav, register_renderer
-from flask_nav.elements import View, Subgroup, Navbar
-from flask_login import LoginManager
+from flask_nav.elements import View, Subgroup, Navbar, Text
+from flask_login import LoginManager, current_user, login_user, login_required, logout_user
+from werkzeug.urls import url_parse
 import config
 
 app = Flask(__name__)
@@ -24,10 +24,12 @@ migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 nav = Nav(app)
 login = LoginManager(app)
+login.login_view = 'login'
 
 import models
 import forms
 from models import User, Feed
+
 
 @app.shell_context_processor
 def make_shell_context():
@@ -40,22 +42,36 @@ class FixedNavBarRenderer(BootstrapRenderer):
         nav_tag['class'] += 'navbar navbar-default navbar-fixed-top'
         return nav_tag
 
+
 register_renderer(app, 'fixed', FixedNavBarRenderer)
+
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+
 @nav.navigation()
 def top_navbar():
     feed_views = [View(t, 'feed', feed_name=t) for t in load_feeds().keys()]
+    user_elements = None
+    if current_user.is_authenticated:
+        user_elements = [
+            Subgroup(
+                "Logged In as '{0}'".format(current_user.username),
+                View('Log Out', 'logout'))
+        ]
+    else:
+        user_elements = [View('Log In', 'login')]
     return Navbar(
         'Kindly',
         View('Home', 'index'),
         Subgroup(
             'Feeds',
-            *feed_views)
+            *feed_views),
+        *user_elements
     )
+
 
 def timed_cache(cache_time):
     def decorator(f):
@@ -77,9 +93,11 @@ def timed_cache(cache_time):
         return cache_wrapper(f)
     return decorator
 
+
 urls = list()
 with app.open_instance_resource('feeds', 'r') as f:
     urls = f.readlines()
+
 
 @timed_cache(timedelta(seconds=10))
 def load_feeds():
@@ -97,18 +115,44 @@ def load_feeds():
 
 @app.route('/')
 @app.route('/index')
+@app.route('/feeds')
+@login_required
 def index():
     return render_template('feed_list.html', feeds=list(load_feeds().values()))
 
+
 @app.route('/feed/<feed_name>')
+@login_required
 def feed(feed_name):
     entries = load_feeds()[feed_name].entries
-    return render_template('feed.html', entries=load_feeds()[feed_name].entries, feed_name=feed_name)
+    return render_template('feed.html', entries=entries, feed_name=feed_name)
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = forms.LoginForm()
-    return render_template('form.html', form=form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('index'))
+        else:
+            login_user(user)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            flash("Logged in as {0}".format(user.username))
+            return redirect(next_page)
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -120,5 +164,4 @@ def register():
         db.session.commit()
         flash('Registration successfull. Please log in.')
         return redirect(url_for('login'))
-    return render_template('form.html', form=form)
-
+    return render_template('register.html', form=form)
--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from kindly import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
@@ -18,7 +18,7 @@ class User(db.Model):
         return '<User {}>'.format(self.username)    
 
 
-class Feed(UserMixin, db.Model):
+class Feed(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
